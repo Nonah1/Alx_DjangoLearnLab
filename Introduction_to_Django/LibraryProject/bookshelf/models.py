e20464345c3940b84d4156ed314826e30e7fcb49
Natasha Wanjiku <natashakathambi47@gmail.com>
2025-09-03 13:39:11 +0300

    Changed files

--- Introduction_to_Django/LibraryProject/bookshelf/models.py
+++ Introduction_to_Django/LibraryProject/bookshelf/models.py
@@ -7,7 +7,7 @@ class Book(models.Model):
     author = models.CharField(max_length=100)
     publication_year = models.IntegerField()
 
-    def _str_(self):
+    def __str__(self):
         return f"{self.title} by {self.author} ({self.publication_year})"
 
 
