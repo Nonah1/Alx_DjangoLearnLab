b24c8071c5e27c21e303140440032e219c1659ae
Natasha Wanjiku <natashakathambi47@gmail.com>
2025-09-05 19:54:51 +0300

    renaming the project

---
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -0,0 +1,20 @@
+from relationship_app.models import Author, Book, Library, Librarian
+
+# 1. Authors with their books
+authors = Author.objects.prefetch_related('books')
+for author in authors:
+    print(author.name)
+    for book in author.books.all():
+        print("   ", book.title)
+
+# 2. Libraries with their books
+libraries = Library.objects.prefetch_related('books')
+for library in libraries:
+    print(library.name)
+    for book in library.books.all():
+        print("   ", book.title)
+
+# 3. Librarians with their libraries
+librarians = Librarian.objects.select_related('library')
+for librarian in librarians:
+    print(librarian.library.name, "-", librarian.name)
