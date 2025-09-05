e1f01c6708791b135a5d1512174de29b4a2ed2d5
Natasha Wanjiku <natashakathambi47@gmail.com>
2025-09-05 16:36:02 +0300

    Adding models and query samples

---
+++ django_models/LibraryProject/bookshelf/admin.py
@@ -0,0 +1,10 @@
+from .models import Book
+from django.contrib import admin
+
+# Register your models here.
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'publication_year')
+    search_fields = ('title', 'author')
+    list_filter = ('title', 'publication_year')
+
+admin.site.register(Book)
