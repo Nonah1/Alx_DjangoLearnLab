41f6bd9920229044f52d5444934e9f1d8cec3cf9
Natasha Wanjiku <natashakathambi47@gmail.com>
2025-09-07 21:26:34 +0300

    adding permissions

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('register/', register, name='register' ),
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout') ,
-    path("books/add/", add_book, name="add_book"),
-    path("books/<int:pk>/edit/", edit_book, name="edit_book"),
-    path("books/<int:pk>/delete/", delete_book, name="delete_book"),
-]
+     # Book permissions (checker requires add_book/ and edit_book/)
+    path("add_book/", add_book, name="add_book"),
+    path("edit_book/<int:pk>/", edit_book, name="edit_book"),
+    path("delete_book/<int:pk>/", delete_book, name="delete_book"),
