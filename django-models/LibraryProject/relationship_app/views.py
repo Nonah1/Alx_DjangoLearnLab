9eacab1a523d6266a4f89134d6f3f6cd8cf22273
Natasha Wanjiku <natashakathambi47@gmail.com>
2025-09-07 19:15:55 +0300

    adding the signup view

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, get_object_or_404
 from django.views.generic.detail import DetailView
 from .models import Library, Book
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
 
 # Create your views here
 def list_books(request):
@@ -13,6 +16,14 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = "library"
+
+class SignUpView(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'relationship_app/register.html'
+
+
+
     
 
        
