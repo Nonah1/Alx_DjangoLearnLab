72ab80613f6472ed5bb8e2ce9e66738920c0e861
Natasha Wanjiku <natashakathambi47@gmail.com>
2025-09-07 19:27:47 +0300

    add register

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path("", list_books, name="home"),   # 👈 root path points to list_books
     path('library_detail/<int:pk>', LibraryDetailView.as_view(), name='library_detail'),
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
-    path('logout/', LogoutView.as_view(), name='logout') 
+    path('register/', views.register, name='register' )
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout') 
 ]
