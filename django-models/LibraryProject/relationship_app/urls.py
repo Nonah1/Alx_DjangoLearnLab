c78b2494be9ffefea1c6cf127a5b10968e70c55f
Natasha Wanjiku <natashakathambi47@gmail.com>
2025-09-07 19:16:39 +0300

    adding user login, logout, registration views urls

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,8 +1,11 @@
 from .views import list_books, LibraryDetailView
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
 urlpatterns = [
     path('books/', list_books, name='books'),
     path("", list_books, name="home"),   # 👈 root path points to list_books
-    path('library_detail/<int:pk>', LibraryDetailView.as_view(), name='library_detail') 
+    path('library_detail/<int:pk>', LibraryDetailView.as_view(), name='library_detail'),
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout') 
 ]
