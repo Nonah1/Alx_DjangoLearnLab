7e91c485723db4d9f990eddab2cfebfaa5c2bf99
Natasha Wanjiku <natashakathambi47@gmail.com>
2025-09-07 17:32:32 +0300

    add

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,8 +1,8 @@
-from relationship_app import views
+from .views import list_books, LibraryView
 from django.urls import path
 
 urlpatterns = [
-    path('books/', views.list_books, name='books'),
-    path("", views.list_books, name="home"),   # 👈 root path points to list_books
-    path('library_detail/<int:pk>', views.LibraryView.as_view(), name='library_detail') 
+    path('books/', list_books, name='books'),
+    path("", list_books, name="home"),   # 👈 root path points to list_books
+    path('library_detail/<int:pk>', LibraryView.as_view(), name='library_detail') 
 ]
