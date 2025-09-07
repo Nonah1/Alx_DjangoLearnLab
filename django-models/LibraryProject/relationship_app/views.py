bcb8f028c086250d0a84b756e7c3ae7145c3fb18
Natasha Wanjiku <natashakathambi47@gmail.com>
2025-09-07 13:30:46 +0300

    fixing views

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,21 +1,19 @@
 from django.shortcuts import render, get_object_or_404
 from django.views.generic.detail import DetailView
-from .models import Library
+from relationship_app.models import Library, Book
 
 # Create your views here
 def list_books(request):
     #Displays list
-    books = Book.objects.all()
-    
+    books = Book.objects.all()    
     context = {"books": books}
-
     return render(request, 'relationship_app/list_books.html', context)
 
 class LibraryView(DetailView):
-    model = Book
+    model = Library
     template_name = 'relationship_app/library_detail.html'
-
-    books = Book.objects.all()
+    context_object_name = "library"
+    
 
        
 
