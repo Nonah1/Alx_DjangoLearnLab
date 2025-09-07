a3c9e2ede858e938bc70c9e5dd8d1270ac349d76
Natasha Wanjiku <natashakathambi47@gmail.com>
2025-09-07 20:47:55 +0300

    RBAC

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -3,10 +3,13 @@ from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
 urlpatterns = [
+    path("admin-view/", admin_view, name="admin_view"),
+    path("librarian-view/", librarian_view, name="librarian_view"),
+    path("member-view/", member_view, name="member_view"),
     path('books/', list_books, name='books'),
     path("", list_books, name="home"),   # 👈 root path points to list_books
     path('library_detail/<int:pk>', LibraryDetailView.as_view(), name='library_detail'),
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
-    path('register/', views.register, name='register' ),
+    path('register/', register, name='register' ),
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout') 
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,12 +1,21 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect
 from django.views.generic.detail import DetailView
 from .models import Library, Book
 from django.contrib.auth.forms import UserCreationForm
-from django.views.generic import CreateView
-from django.urls import reverse_lazy
 from django.contrib.auth import login
+from django.contrib.auth.decorators import user_passes_test
 
 # Create your views here
+# Role checkers
+def is_admin(user):
+    return hasattr(user, "userprofile") and user.userprofile.role == "Admin"
+
+def is_librarian(user):
+    return hasattr(user, "userprofile") and user.userprofile.role == "Librarian"
+
+def is_member(user):
+    return hasattr(user, "userprofile") and user.userprofile.role == "Member"
+
 def list_books(request):
     #Displays list
     books = Book.objects.all()    
@@ -20,7 +29,7 @@ def register(request):
             user = form.save()
             return redirect("login")
     else:
-        form - UserCreationForm()
+        form = UserCreationForm()
     return render(request, 'relationship_app/register.html', {"form": form} )
 
 
@@ -28,13 +37,10 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = "library"
-"""
-class SignUpView(CreateView):
-    form_class = UserCreationForm()
-    success_url = reverse_lazy('login')
-    template_name = 'relationship_app/register.html'
 
-"""
+
+
+
     
 
 
