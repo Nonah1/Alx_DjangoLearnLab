820c857157b9437aa35243513c1fb3cec7ff17ce
Natasha Wanjiku <natashakathambi47@gmail.com>
2025-09-07 19:44:47 +0300

    adding urls and views

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,4 +1,4 @@
-from .views import list_books, LibraryDetailView
+from .views import list_books, LibraryDetailView, register
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
@@ -7,6 +7,6 @@ urlpatterns = [
     path("", list_books, name="home"),   # 👈 root path points to list_books
     path('library_detail/<int:pk>', LibraryDetailView.as_view(), name='library_detail'),
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
-    path('register/', views.register, name='register' )
+    path('register/', register, name='register' ),
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout') 
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -13,17 +13,28 @@ def list_books(request):
     context = {"books": books}
     return render(request, 'relationship_app/list_books.html', context)
 
+def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            return redirect("login")
+    else:
+        form - UserCreationForm()
+    return render(request, 'relationship_app/register.html', {"form": form} )
+
+
 class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = "library"
-
+"""
 class SignUpView(CreateView):
     form_class = UserCreationForm()
     success_url = reverse_lazy('login')
     template_name = 'relationship_app/register.html'
 
-
+"""
     
 
 
